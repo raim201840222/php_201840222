45e061f8a7a69adc112a8dce9110d867ac275dfa
Gwang Min <inobicha@gmail.com>
2019-09-15 23:05:56 +0900

    과제

--- php/for02.php
+++ php/for02.php
@@ -1,9 +1,10 @@
 <?php
     for($i=1;$i<10;$i++){
         if($i%2 == 0){
-            continue;
+            
             echo $i."은 짝수입니다.\n";
         }else {
+            continue;
             echo $i."은 홀수입니다.\n";
         }
     }
--- php/if01.php
+++ php/if01.php
@@ -1,9 +1,9 @@
 <?php 
-    $money = 700;
+    $money = 200;
     if($money >= 1000) {
         echo "콜라\n";
         echo "음료수 사먹는다.";
-    }else if($money < 800) {
+    }else if($money > 800) {
         echo "빵사먹는다.";
 
     }else{
